136db23240e379368740bd8cbd96632828cc9b97
Namdak Tonpa <maxim@synrc.com>
2021-01-06 06:56:54 +0200

    banner

--- src/program.fs
+++ src/program.fs
@@ -4,9 +4,14 @@ module Program =
 
     [<EntryPoint>]
     let main _ =
+        let mutable (workers,io) = (4,4)
         let mutable ret = 0
         let echo = fun x -> x
         try
+            System.Threading.ThreadPool.GetMinThreads(&workers,&io)
+            printfn "N2O/F# WebSocket Server"
+            printfn "[threads] Workers: %i, I/O: %i" workers io
+            System.Threading.ThreadPool.SetMaxThreads(workers,io) |> ignore
             Stream.protocol <- echo
             use disposing = Server.start "0.0.0.0" 1900
             System.Threading.Thread.Sleep -1
