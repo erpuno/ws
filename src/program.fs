8698fcab7a40492ecc2d137d382de883e755fb79
Namdak Tonpa <maxim@synrc.com>
2021-01-06 06:13:17 +0200

    ret

--- src/program.fs
+++ src/program.fs
@@ -4,13 +4,13 @@ module Program =
 
     [<EntryPoint>]
     let main _ =
-        let mutable exitCode = 0
+        let mutable ret = 0
         let echo = fun x -> x
         try
             Stream.protocol <- echo
-            use dispose = Server.start "0.0.0.0" 1900
+            use disposing = Server.start "0.0.0.0" 1900
             System.Threading.Thread.Sleep -1
         with exn ->
             printfn "EXIT: %A" exn.Message
-            exitCode <- 1
-        exitCode
+            ret <- 1
+        ret
