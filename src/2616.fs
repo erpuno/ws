b49cddacc9275072bd2ded39c5de4eb4fc6a1887
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-08-07 19:42:23 +0700

    .NET 4.6

--- src/2616.fs
+++ src/2616.fs
@@ -9,14 +9,14 @@ open System.Collections.Specialized
 module RFC2616 =
 
     let parseHeader (headers : NameValueCollection) (line : string) : unit =
-        match line.Split(':', 2) with
+        match line.Split([| ':' |], 2) with
         | [| key; value |] -> headers.Add(key.ToLower().Trim(), value.Trim())
         | _ -> ()
 
     let request (lines : string array) : Req =
         let req = { path = ""; version = ""; method = ""; headers = NameValueCollection() }
 
-        match (Array.head lines).Split(' ', StringSplitOptions.RemoveEmptyEntries) with
+        match (Array.head lines).Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries) with
         | [| method; uri; version |] ->
             Array.iter (parseHeader req.headers) (Array.tail lines)
             { req with path = uri; version = version; method = method }
